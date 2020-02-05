067c9fdbc71f9c53ad9592a6ea3a2c9c374880f2
benco <20131256@ce.pucmm.edu.do>
2020-02-04 22:56:46 -0400

    udpate our menu items to navigate with react router with rounter HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage.component';
+import {Route, Switch} from 'react-router-dom';
+
+const Hats = () => (
+  <div>
+      Hola
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Switch>
+          <Route exact path='/' component={HomePage}/>
+          <Route exact path='/shop/hats' component={Hats}/>
+      </Switch>
     </div>
   );
 }
